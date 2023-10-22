f886dc810ee4a40a898ce8a8b4ae51815fbdf447
Amol Gadage <AGadage@a10networks.com>
2023-10-22 10:09:04 +0530

    initial commit

---
+++ src/test/java/com/amolsoftwares/www/APIGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amolsoftwares.www.APIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
